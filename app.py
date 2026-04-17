73f9ea45e4197242f58255be393456a073caa0b8
sameeravelluri <sameeravelluri@gmail.com>
2026-04-17 21:21:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,5 +131,8 @@ if __name__ == "__main__":
         db.create_all()
     app.run(debug=True)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
